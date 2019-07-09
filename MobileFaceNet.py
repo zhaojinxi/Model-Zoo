d0d928532885406ae53198a2665bc855fdc87e6e
zhaojnixi <super-string@outlook.com>
2019-07-09 16:54:28 +0800

    6

--- MobileFaceNet.py
+++ MobileFaceNet.py
@@ -1,162 +1,161 @@
 import tensorflow
 
-class MobileNetV2Block(tensorflow.keras.layers.Layer):
-    def __init__(self, stride, t, input_shape, output_shape):
+bias_initializer = tensorflow.keras.initializers.glorot_uniform()
+conv2d_regularizer = None
+depthwise_regularizer = None
+alpha_regularizer = None
+dense_regularizer = None
 
-        super(MobileNetV2Block, self).__init__()
-        self.stride = stride
-        self.t = t
-        self.in_shape = input_shape
-        self.out_shape = output_shape
+def MobileNetV2Block(x, stride, t, output_channel):
+    y = tensorflow.keras.layers.Conv2D(
+        filters=int(t * x.shape.as_list()[3]),
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=conv2d_regularizer,
+        bias_regularizer=conv2d_regularizer)(x)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=conv2d_regularizer,
+        gamma_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.PReLU(
+        alpha_initializer=tensorflow.keras.initializers.Constant(0.25),
+        alpha_regularizer=alpha_regularizer,
+        shared_axes=[1, 2])(y)
 
-        self.Conv2D_1 = tensorflow.keras.layers.Conv2D(
-            filters=self.t * self.in_shape[2],
-            kernel_size=1,
-            strides=1,
-            padding='same')
-        self.BatchNormalization_1 = tensorflow.keras.layers.BatchNormalization()
-        self.PReLU_1 = tensorflow.keras.layers.PReLU()
+    y = tensorflow.keras.layers.DepthwiseConv2D(
+        kernel_size=3,
+        strides=stride,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=depthwise_regularizer,
+        bias_regularizer=depthwise_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=depthwise_regularizer,
+        gamma_regularizer=depthwise_regularizer)(y)
+    y = tensorflow.keras.layers.PReLU(
+        alpha_initializer=tensorflow.keras.initializers.Constant(0.25),
+        alpha_regularizer=alpha_regularizer,
+        shared_axes=[1, 2])(y)
 
-        self.DepthwiseConv2D_2 = tensorflow.keras.layers.DepthwiseConv2D(
-            kernel_size=3,
-            strides=self.stride,
-            padding='same',
-            activation=tensorflow.keras.layers.ReLU(max_value=6))
-        self.BatchNormalization_2 = tensorflow.keras.layers.BatchNormalization()
-        self.PReLU_2 = tensorflow.keras.layers.PReLU()
+    y = tensorflow.keras.layers.Conv2D(
+        filters=output_channel,
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=conv2d_regularizer,
+        bias_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=conv2d_regularizer,
+        gamma_regularizer=conv2d_regularizer)(y)
 
-        self.Conv2D_3 = tensorflow.keras.layers.Conv2D(
-            filters=self.out_shape[2],
-            kernel_size=1,
-            strides=1,
-            padding='same')
-        self.BatchNormalization_3 = tensorflow.keras.layers.BatchNormalization()
+    if x.shape.as_list() == y.shape.as_list():
+        y = tensorflow.keras.layers.Add()([x, y])
+    return y
 
-    def call(self, x):
-        y = self.Conv2D_1(x)
-        y = self.BatchNormalization_1(y)
-        y = self.PReLU_1(y)
+def MobileFaceNet(num_classes):
+    inputs = tensorflow.keras.Input(shape=(112, 112, 3))
 
-        y = self.DepthwiseConv2D_2(y)
-        y = self.BatchNormalization_2(y)
-        y = self.PReLU_2(y)
+    y = tensorflow.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=3,
+        strides=2,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=conv2d_regularizer,
+        bias_regularizer=conv2d_regularizer)(inputs)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=conv2d_regularizer,
+        gamma_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.PReLU(
+        alpha_initializer=tensorflow.keras.initializers.Constant(0.25),
+        alpha_regularizer=alpha_regularizer,
+        shared_axes=[1, 2])(y)
 
-        y = self.Conv2D_3(y)
-        y = self.BatchNormalization_3(y)
+    y = tensorflow.keras.layers.DepthwiseConv2D(
+        kernel_size=3,
+        strides=1,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=depthwise_regularizer,
+        bias_regularizer=depthwise_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=depthwise_regularizer,
+        gamma_regularizer=depthwise_regularizer)(y)
+    y = tensorflow.keras.layers.PReLU(
+        alpha_initializer=tensorflow.keras.initializers.Constant(0.25),
+        alpha_regularizer=alpha_regularizer,
+        shared_axes=[1, 2])(y)
 
-        if self.in_shape == self.out_shape:
-            y = x + y
-        return y
+    y = MobileNetV2Block(y, 2, 2, 64)
+    y = MobileNetV2Block(y, 1, 2, 64)
+    y = MobileNetV2Block(y, 1, 2, 64)
+    y = MobileNetV2Block(y, 1, 2, 64)
+    y = MobileNetV2Block(y, 1, 2, 64)
 
-class MobileFaceNet(tensorflow.keras.Model):
-    def __init__(self, num_classes=1000):
-        super(MobileFaceNet, self).__init__(name='MobileFaceNet')
+    y = MobileNetV2Block(y, 2, 4, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
 
-        self.Conv2D_1 = tensorflow.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=2,
-            padding='same')
-        self.BatchNormalization_1 = tensorflow.keras.layers.BatchNormalization()
-        self.PReLU_1 = tensorflow.keras.layers.PReLU()
+    y = MobileNetV2Block(y, 2, 4, 128)
 
-        self.DepthwiseConv2D_2 = tensorflow.keras.layers.DepthwiseConv2D(
-            kernel_size=3,
-            strides=1,
-            padding='same')
-        self.BatchNormalization_2 = tensorflow.keras.layers.BatchNormalization()
-        self.PReLU_2 = tensorflow.keras.layers.PReLU()
+    y = MobileNetV2Block(y, 1, 2, 128)
+    y = MobileNetV2Block(y, 1, 2, 128)
 
-        self.MobileNetV2Block_3 = MobileNetV2Block(2, 2, [56, 56, 64], [28, 28, 64])
-        self.MobileNetV2Block_4 = MobileNetV2Block(1, 2, [28, 28, 64], [28, 28, 64])
-        self.MobileNetV2Block_5 = MobileNetV2Block(1, 2, [28, 28, 64], [28, 28, 64])
-        self.MobileNetV2Block_6 = MobileNetV2Block(1, 2, [28, 28, 64], [28, 28, 64])
-        self.MobileNetV2Block_7 = MobileNetV2Block(1, 2, [28, 28, 64], [28, 28, 64])
+    y = tensorflow.keras.layers.Conv2D(
+        filters=512,
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        bias_initializer=bias_initializer,
+        kernel_regularizer=conv2d_regularizer,
+        bias_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=conv2d_regularizer,
+        gamma_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.PReLU(
+        alpha_initializer=tensorflow.keras.initializers.Constant(0.25),
+        alpha_regularizer=alpha_regularizer,
+        shared_axes=[1, 2])(y)
 
-        self.MobileNetV2Block_8 = MobileNetV2Block(2, 4, [28, 28, 64], [14, 14, 128])
+    y = tensorflow.keras.layers.DepthwiseConv2D(
+        kernel_size=7,
+        strides=1,
+        bias_initializer=bias_initializer,
+        kernel_regularizer=depthwise_regularizer,
+        bias_regularizer=depthwise_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=depthwise_regularizer,
+        gamma_regularizer=depthwise_regularizer)(y)
 
-        self.MobileNetV2Block_9 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
-        self.MobileNetV2Block_10 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
-        self.MobileNetV2Block_11 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
-        self.MobileNetV2Block_12 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
-        self.MobileNetV2Block_13 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
-        self.MobileNetV2Block_14 = MobileNetV2Block(1, 2, [14, 14, 128], [14, 14, 128])
+    y = tensorflow.keras.layers.Conv2D(
+        filters=128,
+        kernel_size=1,
+        strides=1,
+        bias_initializer=bias_initializer,
+        kernel_regularizer=conv2d_regularizer,
+        bias_regularizer=conv2d_regularizer)(y)
+    y = tensorflow.keras.layers.BatchNormalization(
+        beta_regularizer=conv2d_regularizer,
+        gamma_regularizer=conv2d_regularizer)(y)
+    embed = tensorflow.keras.layers.Flatten()(y)
 
-        self.MobileNetV2Block_15 = MobileNetV2Block(2, 4, [14, 14, 128], [7, 7, 128])
+    predict = tensorflow.keras.layers.Dense(
+        units=num_classes,
+        use_bias=False,
+        name='last_layer',
+        kernel_regularizer=dense_regularizer)(embed)
 
-        self.MobileNetV2Block_16 = MobileNetV2Block(1, 2, [7, 7, 128], [7, 7, 128])
-        self.MobileNetV2Block_17 = MobileNetV2Block(1, 2, [7, 7, 128], [7, 7, 128])
-
-        self.Conv2D_18 = tensorflow.keras.layers.Conv2D(
-            filters=512,
-            kernel_size=1,
-            strides=1,
-            padding='same')
-        self.BatchNormalization_18 = tensorflow.keras.layers.BatchNormalization()
-        self.PReLU_18 = tensorflow.keras.layers.PReLU()
-
-        self.DepthwiseConv2D_19 = tensorflow.keras.layers.DepthwiseConv2D(
-            kernel_size=7,
-            strides=1)
-        self.BatchNormalization_19 = tensorflow.keras.layers.BatchNormalization()
-
-        self.Conv2D_20 = tensorflow.keras.layers.Conv2D(
-            filters=128,
-            kernel_size=1,
-            strides=1,
-            padding='same')
-        self.BatchNormalization_20 = tensorflow.keras.layers.BatchNormalization()
-
-        self.Conv2D_21 = tensorflow.keras.layers.Conv2D(
-            filters=num_classes,
-            kernel_size=1,
-            strides=1,
-            padding='same')
-
-    def call(self, x):
-        x = self.Conv2D_1(x)
-        x = self.BatchNormalization_1(x)
-        x = self.PReLU_1(x)
-
-        x = self.DepthwiseConv2D_2(x)
-        x = self.BatchNormalization_2(x)
-        x = self.PReLU_2(x)
-
-        x = self.MobileNetV2Block_3(x)
-        x = self.MobileNetV2Block_4(x)
-        x = self.MobileNetV2Block_5(x)
-        x = self.MobileNetV2Block_6(x)
-        x = self.MobileNetV2Block_7(x)
-
-        x = self.MobileNetV2Block_8(x)
-
-        x = self.MobileNetV2Block_9(x)
-        x = self.MobileNetV2Block_10(x)
-        x = self.MobileNetV2Block_11(x)
-        x = self.MobileNetV2Block_12(x)
-        x = self.MobileNetV2Block_13(x)
-        x = self.MobileNetV2Block_14(x)
-
-        x = self.MobileNetV2Block_15(x)
-
-        x = self.MobileNetV2Block_16(x)
-        x = self.MobileNetV2Block_17(x)
-
-        x = self.Conv2D_18(x)
-        x = self.BatchNormalization_18(x)
-        x = self.PReLU_18(x)
-
-        x = self.DepthwiseConv2D_19(x)
-        x = self.BatchNormalization_19(x)
-
-        x = self.Conv2D_20(x)
-        x = self.BatchNormalization_20(x)
-
-        x = self.Conv2D_21(x)
-        return x
+    model = tensorflow.keras.Model(inputs=inputs, outputs=[embed, predict])
+    return model
 
 if __name__ == '__main__':
-    tensorflow.enable_eager_execution()
-    data = tensorflow.random.uniform([128, 112, 112, 3])
-    model = MobileFaceNet()
-    y = model(data)
+    tensorflow.enable_eager_execution(config=tensorflow.ConfigProto(allow_soft_placement=True, gpu_options=tensorflow.GPUOptions(allow_growth=True)))
+    data = tensorflow.random.uniform([4, 112, 112, 3])
+    model = MobileFaceNet(85742)
+    embed = model(data)
